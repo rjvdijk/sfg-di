6425bb3a395fee92837f03873163195c3070b58c
Rob van Dijk <rob@optimust.nl>
2020-07-02 11:28:38 +0200

    37. Primary Beans

--- src/main/java/nl/optimust/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/nl/optimust/springframework/sfgdi/controllers/MyController.java
@@ -1,13 +1,18 @@
 package nl.optimust.springframework.sfgdi.controllers;
 
+import nl.optimust.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
-    public String sayHello() {
-        System.out.println("Hello World!");
+    private final GreetingService greetingService;
 
-        return "Hi Folks!";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
     }
 }
