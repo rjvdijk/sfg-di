8ae803bb37d251bb6fee16d93dfe2e563c83cfb7
Rob van Dijk <rob@optimust.nl>
2020-07-02 11:51:38 +0200

    38. Spring Profiles

--- src/main/java/nl/optimust/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/nl/optimust/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package nl.optimust.springframework.sfgdi;
 
-import nl.optimust.springframework.sfgdi.controllers.ConstructedInjectedController;
-import nl.optimust.springframework.sfgdi.controllers.MyController;
-import nl.optimust.springframework.sfgdi.controllers.PropertyInjectedController;
-import nl.optimust.springframework.sfgdi.controllers.SetterInjectedController;
+import nl.optimust.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,28 +11,24 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
+		System.out.println("------ ");
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+
 		System.out.println("------ Primary Bean");
-
 		MyController myController = (MyController) ctx.getBean("myController");
-
 		System.out.println(myController.sayHello());
 
 		System.out.println("------ Property");
-
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-
 		System.out.println(propertyInjectedController.getGreeting());
 
 		System.out.println("------ Setter");
-
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-
 		System.out.println(setterInjectedController.getGreeting());
 
 		System.out.println("------ Constructor");
-
 		ConstructedInjectedController constructedInjectedController = (ConstructedInjectedController) ctx.getBean("constructedInjectedController");
-
 		System.out.println(constructedInjectedController.getGreeting());
 	}
 
---
+++ src/main/java/nl/optimust/springframework/sfgdi/controllers/I18nController.java
@@ -0,0 +1,20 @@
+package nl.optimust.springframework.sfgdi.controllers;
+
+import nl.optimust.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Controller
+public class I18nController {
+
+    private final GreetingService greetingService;
+
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
